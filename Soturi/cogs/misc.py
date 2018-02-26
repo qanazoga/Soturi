570a090fcc78e3a053bc9dfc8f077742be147270
Paige <qanazoga@gmail.com>
2018-02-25 19:58:39 -0700

    Added magic conch

--- Soturi/cogs/misc.py
+++ Soturi/cogs/misc.py
@@ -21,6 +21,32 @@ class Misc:
     async def choice(self, ctx: commands.Context, *args):
         await ctx.send(random.choice(args))
 
+    @commands.command(aliases=['8ball', '8_ball', 'eightball', 'magic_conch'])
+    async def eight_ball(self, ctx: commands.Context):
+        """Let the magic eight ball decide your fate!"""
+        await ctx.send(random.choice([
+            "It is certain",
+            "It is decidedly so",
+            "Without a doubt",
+            "Yes definitely",
+            "You may rely on it",
+            "As I see it, yes",
+            "Most likely",
+            "Outlook good",
+            "Yes",
+            "Signs point to yes",
+            "Reply hazy try again",
+            "Ask again later",
+            "Better not tell you now",
+            "Cannot predict now",
+            "Concentrate and ask again",
+            "Don't count on it",
+            "My reply is no",
+            "My sources say no",
+            "Outlook not so good",
+            "Very doubtful"
+        ]))
+
 
 def setup(bot):
     bot.add_cog(Misc(bot))
